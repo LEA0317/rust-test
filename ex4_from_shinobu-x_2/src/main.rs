f920fcd3231724f0d0db0795d227fc9d75c911b7
toshihiro konda <kontoshi0317@gmail.com>
2020-07-01 19:19:35 +0900

    refactoring

--- ex4_from_shinobu-x_2/src/main.rs
+++ ex4_from_shinobu-x_2/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     const INF: usize = 1_000;
+    const M: i32 = 10_000;
     let n: i32 = 4;
     let m: i32 = 3;
-    let _m: i32 = 10_000;
     let mut dp = [[0 as i32; INF]; INF];
 
     dp[0][0] = 1;
     for i in 1..m+1 {
         for j in 0..n+1 {
        	    if j - i >= 0 {
-       	        dp[i as usize][j as usize] = (dp[(i - 1) as usize][j as usize] + dp[i as usize][(j - i) as usize]) % _m;
+       	        dp[i as usize][j as usize] = (dp[(i - 1) as usize][j as usize] + dp[i as usize][(j - i) as usize]) % M;
        	    } else {
        	        dp[i as usize][j as usize] = dp[(i - 1) as usize][j as usize];
        	    }
